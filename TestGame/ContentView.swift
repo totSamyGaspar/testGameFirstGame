33214311e281be097e57c18d7d4aadffbd901318
Edward Gasparian <gilberd@i.ua>
2025-10-03 15:05:31 +0400

    Initial Commit

---
+++ TestGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestGame
+//
+//  Created by Edward Gasparian on 03.10.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestGame/TestGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGameApp.swift
+//  TestGame
+//
+//  Created by Edward Gasparian on 03.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct TestGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
